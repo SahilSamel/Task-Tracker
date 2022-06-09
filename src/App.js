f6e9eb5ec9aaf9cb06a20f2ea7a6ff9f554bd4ac
Sahil Samel <sahilsamel134@gmail.com>
2022-06-09 12:22:43 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header.js'
+
+function App() {
+  const name='sahil'
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
