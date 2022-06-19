f7265a03a44791b45b50bebf4b31d650cf14ed8c
Sahil Samel <sahilsamel134@gmail.com>
2022-06-19 15:52:00 +0530

    Working Front End

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import Header from './components/header.js'
+import Tasks from './components/tasks'
+import AddTask from './components/addTask'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const name='sahil'
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () =>{
+      const tasksServer = await fetchTasks()
+      setTasks(tasksServer)
+    }
+
+    getTasks()
+  },[])
+
+  const fetchTasks = async () =>{
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+    
+    return data
+  }
+
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      header:{
+        'Content-type': 'application/json'
+      },
+      body:JSON.stringify(task)
+    })
+    const data = await res.json()
+    setTasks([...tasks,data])
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const togggleReminder = (id) => {
+    setTasks(tasks.map((task)=> task.id===id ? {...task,reminder:!task.reminder} : task))
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div className="container">
+      <Header onAdd ={() =>setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={togggleReminder}/> : 'No tasks'}
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
-const Header = () => {
+import Button from "./button";
+
+const Header = ({title,onAdd,showAddTask}) => {
   return (
-    <header>
-        <h1>Task Tracker</h1>
+    <header className="header">
+        <h1>Yo.</h1>
+        <Button color={showAddTask? '#9C6464':'#75e6dc'} text={showAddTask ? "Done":"Add Tasks"} onClick = {onAdd}/>
     </header>
   );
 }
